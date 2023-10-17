95e9732110492cb693332da3c8bfa3d81d50cf0d
Maxamed Maxamed <20088349@mail.wit.ie>
2023-10-17 14:19:03 +0100

    Change site header.2

--- movies/src/index.js
+++ movies/src/index.js
@@ -160,12 +160,12 @@
 import MovieReviewPage from "./pages/movieReviewPage";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes,  } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import SiteHeader from './components/siteHeader'
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 
 const App = () => {
   return (
